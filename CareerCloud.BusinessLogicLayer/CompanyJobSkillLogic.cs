983c6be2280efd2106055f9d4949a4a92df7788c
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2019-12-30 21:10:55 -0500

    Boilerplate for CompanyJobSkillLogic

---
+++ CareerCloud.BusinessLogicLayer/CompanyJobSkillLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyJobSkillLogic : BaseLogic<CompanyJobSkillPoco>
+    {
+        public CompanyJobSkillLogic(IDataRepository<CompanyJobSkillPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyJobSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            foreach (CompanyJobSkillPoco poco in pocos)
+            {
+                
+            }
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyJobSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyJobSkillPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
