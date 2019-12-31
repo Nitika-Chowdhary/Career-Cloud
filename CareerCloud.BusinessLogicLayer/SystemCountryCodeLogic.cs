86b7a839a48f2a3381c9a564623458654850f9c4
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2019-12-30 21:21:50 -0500

    Boilerplate for SystemCountryCodeLogic

---
+++ CareerCloud.BusinessLogicLayer/SystemCountryCodeLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class SystemCountryCodeLogic : BaseLogic<SystemCountryCodePoco>
+    {
+        public SystemCountryCodeLogic(IDataRepository<SystemCountryCodePoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(SystemCountryCodePoco[] pocos)
+        {
+            Verify(pocos);
+            foreach (SystemCountryCodePoco poco in pocos)
+            {
+                
+            }
+            base.Add(pocos);
+        }
+
+        public override void Update(SystemCountryCodePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(SystemCountryCodePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
