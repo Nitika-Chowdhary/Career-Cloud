f8c2067ca38dd43e1c6534bdf9702d43c7e0f4fb
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2019-12-30 21:15:39 -0500

    Boilerplate for CompanyLocationLogic

---
+++ CareerCloud.BusinessLogicLayer/CompanyLocationLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyLocationLogic : BaseLogic<CompanyLocationPoco>
+    {
+        public CompanyLocationLogic(IDataRepository<CompanyLocationPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyLocationPoco[] pocos)
+        {
+            Verify(pocos);
+            foreach (CompanyLocationPoco poco in pocos)
+            {
+                
+            }
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyLocationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyLocationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
