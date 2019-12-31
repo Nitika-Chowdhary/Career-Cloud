2628138a6a73026a0feca8abd29744b758cd101a
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2019-12-30 21:08:54 -0500

    Boilerplate for CompanyJobEducationLogic

---
+++ CareerCloud.BusinessLogicLayer/CompanyJobEducationLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyJobEducationLogic : BaseLogic<CompanyJobEducationPoco>
+    {
+        public CompanyJobEducationLogic(IDataRepository<CompanyJobEducationPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyJobEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            foreach (CompanyJobEducationPoco poco in pocos)
+            {
+                
+            }
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyJobEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyJobEducationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
