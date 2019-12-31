238fa11f04217b59402e446e77171ca33c08584a
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2019-12-30 20:40:48 -0500

    Boilerplate for ApplicantJobApplicationLogic

---
+++ CareerCloud.BusinessLogicLayer/ApplicantJobApplicationLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantJobApplicationLogic : BaseLogic<ApplicantJobApplicationPoco>
+    {
+        public ApplicantJobApplicationLogic(IDataRepository<ApplicantJobApplicationPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(ApplicantJobApplicationPoco[] pocos)
+        {
+            Verify(pocos);
+            foreach (ApplicantJobApplicationPoco poco in pocos)
+            {
+                
+            }
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantJobApplicationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantJobApplicationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
