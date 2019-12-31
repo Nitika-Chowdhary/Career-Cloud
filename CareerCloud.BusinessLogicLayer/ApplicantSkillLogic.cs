c915e57950d288071fdea18e1e8064c91adcb0c3
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2019-12-30 20:54:38 -0500

    Boilerplate for ApplicantSkillLogic

---
+++ CareerCloud.BusinessLogicLayer/ApplicantSkillLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantSkillLogic : BaseLogic<ApplicantSkillPoco>
+    {
+        public ApplicantSkillLogic(IDataRepository<ApplicantSkillPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(ApplicantSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            foreach (ApplicantSkillPoco poco in pocos)
+            {
+                
+            }
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantSkillPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantSkillPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
