d39dde1fa0a9404b9895c4d951adf61ac37f1d73
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2019-12-30 21:01:32 -0500

    Boilerplate for ApplicantWorkHistoryLogic

---
+++ CareerCloud.BusinessLogicLayer/ApplicantWorkHistoryLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantWorkHistorysLogic : BaseLogic<ApplicantWorkHistorysPoco>
+    {
+        public ApplicantWorkHistorysLogic(IDataRepository<ApplicantWorkHistorysPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(ApplicantWorkHistorysPoco[] pocos)
+        {
+            Verify(pocos);
+            foreach (ApplicantWorkHistorysPoco poco in pocos)
+            {
+                
+            }
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantWorkHistorysPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantWorkHistorysPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
