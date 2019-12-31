5bd4bb5ddd16cd4ae24d3663a55cca91a754bf89
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2019-12-30 21:18:32 -0500

    Boilerplate for CompanyProfileLogic

---
+++ CareerCloud.BusinessLogicLayer/CompanyProfileLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyProfileLogic : BaseLogic<CompanyProfilePoco>
+    {
+        public CompanyProfileLogic(IDataRepository<CompanyProfilePoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            foreach (CompanyProfilePoco poco in pocos)
+            {
+                
+            }
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyProfilePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
