3c95f2d0bd4ced6dfe1718203d548641bbb4fde1
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2019-12-30 21:03:18 -0500

    Boilerplate for CompanyDescriptionLogic

---
+++ CareerCloud.BusinessLogicLayer/CompanyDescriptionLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyDescriptionLogic : BaseLogic<CompanyDescriptionPoco>
+    {
+        public CompanyDescriptionLogic(IDataRepository<CompanyDescriptionPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            foreach (CompanyDescriptionPoco poco in pocos)
+            {
+                
+            }
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyDescriptionPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyDescriptionPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
